95c588ffdaa53cc72db59c9ca3a068196b04c533
Rafa <rafa.lozano.rolon@gmail.com>
2020-01-12 17:25:16 -0600

    Se agrego cambio valor array dentro de una estructura

--- functions/StructureAsParameter/callByAddress.c
+++ functions/StructureAsParameter/callByAddress.c
@@ -3,18 +3,23 @@ struct Rectangle
 {
     int lenght;
     int breadth;
+    int array[2];
 };
 void ChangeLength(struct Rectangle *p, int newlength)
 {
     p->lenght = newlength;
 }
+void changeArray(struct Rectangle *p,int value)
+{
+    p->array[0]=value;
+}
 
 int main()
 {
-    struct Rectangle r1 = {10, 2};
-    printf("Before ChangeLength:%d \n",r1.lenght);
-    ChangeLength(&r1, 25);
-    printf("After ChangeLength:%d \n", r1.lenght);
+    struct Rectangle r1 = {10, 2,{3,6}};
+    printf("Before ChangeLength:%d \n",r1.array[0]);
+    changeArray(&r1, 25);
+    printf("After ChangeLength:%d \n", r1.array[0]);
 
     return 0;
 }
